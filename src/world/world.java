83a4c6bbad8f71d06d851656bb900711748e5147
Epyke <epicsniper278@gmail.com>
2025-11-30 19:45:50 +0000

    start of world class

---
+++ src/world/world.java
@@ -0,0 +1,12 @@
+package world;
+
+import java.util.ArrayList;
+
+public class world {
+    private static final int size = 50;
+    private ArrayList<ArrayList<Integer>> grid;
+
+    public world(){
+        grid = new ArrayList<>(size);
+    }
+}
